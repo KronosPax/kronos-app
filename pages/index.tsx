855602ad8703f82b18c98f41672ee0fb646428eb
Ross0-0 <ross.james94@gmail.com>
2022-10-25 10:17:19 -0500

    created login layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,42 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import {Box, Button, Checkbox, FormControl, FormLabel, Heading, HStack, Input, Text, VStack} from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>KronosPax</title>
-        <meta name="description" content="KronosPax Calender" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to KronosPax!
-        </h1>
-      </main>
-    </div>
+          <Box
+              w={['full', 'md']}
+              p={[8,10]}
+              mt={[20, '10vh']}
+              mx='auto'
+              border={['none', '1px']}
+              borderColor={['', 'gray.300']}
+              borderRadius={10}
+          >
+              <VStack spacing={4} align={'flex-start'} w={'full'}>
+                  <VStack spacing={1} w={'full'}>
+                      <Heading>Login to KronosPax</Heading>
+                      <Text>Enter your email and password</Text>
+                  </VStack>
+
+                  <FormControl>
+                      <FormLabel>Email Address</FormLabel>
+                      <Input rounded='none' variant={'filled'} type={'email'} />
+                  </FormControl>
+                  <FormControl>
+                      <FormLabel>Password</FormLabel>
+                      <Input rounded='none' variant={'filled'} type={'password'} />
+                  </FormControl>
+                  <HStack w={'full'} justify={'space-between'}>
+                      <Checkbox>Remember me</Checkbox>
+                      <Button variant={'link'} colorScheme={'blue'}>
+                          Forgot Password
+                      </Button>
+                  </HStack>
+                  <Button rounded={'none'} colorScheme={'blue'} w={['full', 'auto']}>
+                      Login
+                  </Button>
+              </VStack>
+          </Box>
   )
 }
 
